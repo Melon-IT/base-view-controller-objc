5239bdaad26de041f5fb12e43f17206f4d56ff7d
Tomek Popis <tomek@melon-it.com>
2016-02-07 17:45:40 +0100

    Configure project

---
+++ MelonBaseViewController/MelonBaseViewController/MelonBaseViewController.h
@@ -0,0 +1,19 @@
+//
+//  MelonBaseViewController.h
+//  MelonBaseViewController
+//
+//  Created by Tomasz Popis on 07/02/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MelonBaseViewController.
+FOUNDATION_EXPORT double MelonBaseViewControllerVersionNumber;
+
+//! Project version string for MelonBaseViewController.
+FOUNDATION_EXPORT const unsigned char MelonBaseViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MelonBaseViewController/PublicHeader.h>
+
+
